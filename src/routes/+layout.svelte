63cca9ab16f76a4273c8b0187632a5323ff0172c
pyxus <eze.halley@gmail.com>
2023-12-20 02:34:33 -0500

    Add page transition

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,12 +9,16 @@
 		getDrawerStore
 	} from '@skeletonlabs/skeleton';
 	import 'iconify-icon';
+	import PageTransitionLayout from './PageTransitionLayout.svelte';
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { storePopup } from '@skeletonlabs/skeleton';
 	import SocialsButton from './SocialsButton.svelte';
 	import NavList from './NavList.svelte';
+
+	export let data;
+
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
 	initializeStores();
@@ -42,37 +46,32 @@
 </Drawer>
 <AppShell>
 	<svelte:fragment slot="pageHeader">
-		<!-- App Bar -->
 		<nav>
-			<div>
-				<AppBar
-					slotDefault="place-self-center"
-					slotTrail="place-content-end"
-					padding="lg:pl-60 lg:pr-60 pr-4 pl-4 pt-3 pb-3"
-				>
-					<svelte:fragment slot="lead">
-						<div class="flex items-center">
-							<button
-								class="btn mr-4 text-2xl w-auto md:hidden variant-filled"
-								on:click={drawerOpen}
-							>
-								<iconify-icon icon="ci:hamburger-md" />
-							</button>
-							<a href="/"><strong class="text-xl uppercase">Pyxus.dev</strong></a>
-						</div>
-					</svelte:fragment>
-					<NavList {routes} />
-					<svelte:fragment slot="trail">
-						<LightSwitch rounded="rounded-md md:flex hidden" />
-						<SocialsButton container="md:flex hidden" />
-					</svelte:fragment>
-				</AppBar>
-			</div>
+			<AppBar
+				slotDefault="place-self-center"
+				slotTrail="place-content-end"
+				padding="lg:pl-60 lg:pr-60 pr-4 pl-4 pt-3 pb-3"
+			>
+				<svelte:fragment slot="lead">
+					<div class="flex items-center">
+						<button class="btn mr-4 text-2xl w-auto md:hidden variant-filled" on:click={drawerOpen}>
+							<iconify-icon icon="ci:hamburger-md" />
+						</button>
+						<a href="/"><strong class="text-xl uppercase">Pyxus.dev</strong></a>
+					</div>
+				</svelte:fragment>
+				<NavList {routes} />
+				<svelte:fragment slot="trail">
+					<LightSwitch rounded="rounded-md md:flex hidden" />
+					<SocialsButton container="md:flex hidden" />
+				</svelte:fragment>
+			</AppBar>
 		</nav>
 	</svelte:fragment>
 	<svelte:fragment slot="sidebarLeft"></svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
+	<PageTransitionLayout url={data.url}>
+		<slot />
+	</PageTransitionLayout>
 </AppShell>
 
 <style>
