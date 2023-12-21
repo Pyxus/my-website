aeb7c21bcfcfc5d57efcc56316dc7f1d72fa052a
pyxus <eze.halley@gmail.com>
2023-12-20 23:45:51 -0500

    Add remark and rehype plugins

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,30 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import { mdsvex } from 'mdsvex';
+import { mdsvex, escapeSvelte } from 'mdsvex';
+import shiki from 'shiki'
+import remarkUnwrapImages from 'remark-unwrap-images'
+import rehypeSlug from 'rehype-slug';
+
+const mdsvexOptions = {
+	extensions: ['.md'],
+	remarkPlugins: [remarkUnwrapImages],
+	rehypePlugins: [rehypeSlug],
+	highlight: {
+		highlighter: async (code, lang = 'text') => {
+			const highlighter = await shiki.getHighlighter({
+				theme: 'poimandres', //TODO: Change theme, also look into shiki twoslash
+			})
+
+			const html = escapeSvelte(highlighter.codeToHtml(code, { lang }))
+			return `{@html \`${html}\`}`
+		}
+	},
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
-	preprocess: [ vitePreprocess(), mdsvex({extensions: ['.md']})],
+	preprocess: [ vitePreprocess(), mdsvex(mdsvexOptions)],
 	vitePlugin: {
 		inspector: true,
 	},
