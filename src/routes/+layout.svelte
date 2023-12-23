72a33af54f2f58cde4697573ee03e2d3b66973c2
pyxus <eze.halley@gmail.com>
2023-12-23 15:56:51 -0500

    Improve drawer layout
    drawer and menu button now exist on right for easier access during mobile view

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -37,12 +37,23 @@
 	function drawerOpen(): void {
 		drawerStore.open();
 	}
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
 </script>
 
-<Drawer>
-	<NavList useFlex={false} {routes} />
-	<LightSwitch rounded="rounded-md" />
-	<SocialsButton />
+<Drawer position="right">
+	<div class="flex flex-col p-5 gap-5">
+		<div class="w-full">
+			<button on:click={drawerClose} class="float-right">
+				<iconify-icon class="text-4xl" icon="zondicons:close-solid" />
+			</button>
+		</div>
+		<NavList useFlex={false} {routes} />
+		<LightSwitch rounded="rounded-md" />
+		<SocialsButton />
+	</div>
 </Drawer>
 <AppShell scrollbarGutter="stable" regionPage="relative scroll-smooth">
 	<svelte:fragment slot="header">
@@ -53,15 +64,13 @@
 				padding="lg:pl-60 lg:pr-60 pr-4 pl-4 pt-3 pb-3"
 			>
 				<svelte:fragment slot="lead">
-					<div class="flex items-center">
-						<button class="btn mr-4 text-2xl w-auto md:hidden variant-filled" on:click={drawerOpen}>
-							<iconify-icon icon="ci:hamburger-md" />
-						</button>
-						<a href="/"><strong class="text-xl uppercase">{title}</strong></a>
-					</div>
+					<a href="/"><strong class="text-xl uppercase">{title}</strong></a>
 				</svelte:fragment>
 				<NavList {routes} />
 				<svelte:fragment slot="trail">
+					<button class="btn mr-4 text-2xl w-auto md:hidden variant-filled" on:click={drawerOpen}>
+						<iconify-icon icon="ci:hamburger-md" />
+					</button>
 					<LightSwitch rounded="rounded-md md:flex hidden" />
 					<SocialsButton container="md:flex hidden" />
 				</svelte:fragment>
