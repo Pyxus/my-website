c43c1e04c4420bc86693805f8722e8aeac3a8bf3
pyxus <eze.halley@gmail.com>
2024-01-18 07:37:20 -0500

    Show art title on hover

--- src/routes/art/+page.svelte
+++ src/routes/art/+page.svelte
@@ -16,8 +16,20 @@
 	{#each art as artwork}
 		<Lazy height={400} placeholder={PlaceholderImage} keep="true" fadeOption={{ duration: 400 }}>
 			<a target="_blank" href={artwork.src}>
-				<figure class="transition hover:scale-105">
-					<img class="h-auto max-w-full rounded-lg" src={artwork.src} alt={artwork.meta.alt} />
+				<figure class="relative overflow-hidden group col-span-2">
+					<img
+						class="transition-transform group-hover:scale-110 h-auto max-w-full rounded-lg duration-300"
+						src={artwork.src}
+						alt={artwork.meta.alt}
+					/>
+					<div
+						class="absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-50 transition-opacity duration-300"
+					></div>
+					<figcaption
+						class="absolute inset-x-0 bottom-0 p-4 text-white text-center opacity-0 group-hover:opacity-100"
+					>
+						{artwork.meta.title}
+					</figcaption>
 				</figure>
 			</a>
 		</Lazy>
